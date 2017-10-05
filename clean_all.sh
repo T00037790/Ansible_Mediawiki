f0054abb981095280d53f1dc057b694fb7726981
T00037790 <izzymovil@gmail.com>
2017-10-05 13:22:17 -0500

    folders creation

---
+++ clean_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Detediendo docker"
+docker stop server01 
+echo "Docker detenido"
+echo
+echo "----------------------"
+echo "Eliminando docker"
+docker rm server01 
+echo "Docker eliminado."
+echo
+echo "----------------------"
+echo "Limpieza lista."
+echo
+echo "----------------------"
+echo "Recreando dockers"
+./create_dockers.sh server:16.04
+
+ssh -o StrictHostKeyChecking=no root@server01 -p 2221 -i key.private hostname
+echo "Adicionada la llave ssh."
+echo
+echo "----------------------"
+echo "Sigue con ansible001"
